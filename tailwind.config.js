16312373d5665ce1301a565f979e9005dc00253c
sathish060799 <wimpysathish@gmail.com>
2023-10-11 00:38:13 +0800

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'home-background': "url('/background.png`)",
+      },
+      fontFamily: {
+        'sans': ['Open Sans', 'sans-serif'],
       },
     },
   },
