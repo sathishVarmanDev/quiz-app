3a724202a110794c00d8323a944cc8af05deebeb
sathish060799 <wimpysathish@gmail.com>
2023-10-12 01:36:09 +0800

    revised mobile & desktop

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from "@/styles/Home.module.css"
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -18,6 +19,14 @@ export default function Home() {
           </button>
         </div>
       </div>
+        <div className={`${styles.statisticsContainer}`}>
+          <Image alt='statistics' unoptimized width={100} height={100} src={`/statistics.png`} className={`${styles.statisticsImage}`} />
+        </div>
+        <div className={`${styles.resultsContainer}`}>
+          <Image alt='results' unoptimized width={100} height={100} src={`/resultsTwo.png`} className={`${styles.resultsImage}`} />
+      </div>
+      {/* <Image alt='right-banner' unoptimized width={100} height={100} src={`/rightBanner.png`} className={`${styles.rightBanner}`} /> */}
+      <Image alt='results-right' unoptimized width={100} height={100} src={`/resultsRight.png`} className={`${styles.resultsRightImage}`} />
     </div>
   )
 }
